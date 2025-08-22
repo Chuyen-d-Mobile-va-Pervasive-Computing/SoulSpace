e59521cbe62c06e05c091f137bf6bcdadb1ca78e
DangThiNgocMinh <minhdang@cybertech.com.vn>
2025-08-22 12:29:18 +0700

    navigate tabs

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function LoginScreen() {
+  const goToTabs = () => {
+    // điều hướng sang group (tabs), mặc định sẽ vào tab đầu tiên (home)
+    router.replace("/(tabs)/home");
+  };
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-inter_bold text-3xl">SOULSPACE</Text>
+      <Text className="font-inter_bold text-3xl mb-6">SOULSPACE</Text>
+
+      <TouchableOpacity
+        onPress={goToTabs}
+        className="px-6 py-3 bg-[#6F04D9] rounded-xl"
+      >
+        <Text className="text-white font-semibold text-lg">Vào ứng dụng</Text>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      <Text className="text-xl font-bold">🏠 Đây là màn hình Home</Text>
+    </View>
+  );
+}
